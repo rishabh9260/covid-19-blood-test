46829abafc0dad14f330338b8c94974540a2bf4e
Param Chauhan <paramchauhan21@gmail.com>
2021-06-01 10:00:39 +0530

    Add float, cache and probability. Refactor.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,69 +5,76 @@ import xgboost
 
 st.title("Covid-19 Prediction using Blood Test")
 
-NRS = ['Note Red Series_Anisocitose +',
-       'Note Red Series_Anisocitose ++', 'Note Red Series_Anisocitose +++',
-       'Note Red Series_Eritrócitos normais em tamanho',
-       'Note Red Series_Macrocitose +', 'Note Red Series_Macrocitose ++',
-       'Note Red Series_Macrocitose +++', 'Note Red Series_Microcitose +',
-       'Note Red Series_Microcitose ++', 'Note Red Series_Microcitose +++']
-
-with st.form("my_form"):
+with st.form("blood_test_form"):
     col1, col2, col3, col4 = st.beta_columns(4)
     with col1:
-        hcm = st.number_input("HCM", 0, 100, 0)
-        hemoglobin = st.number_input("Hemoglobin", 0, 100, 0)
-        mchc = st.number_input("MCHC", 0, 100, 0)
-        rdw_cv = st.number_input("RDW-CV", 0, 100, 0)
+        hcm = st.number_input(label="HCM", min_value=0.0, max_value=1000.0, value=0.0)
+        hemoglobin = st.number_input(label="Hemoglobin", min_value=0.0, max_value=1000.0, value=0.0)
+        mchc = st.number_input(label="MCHC", min_value=0.0, max_value=1000.0, value=0.0)
+        rdw_cv = st.number_input(label="RDW-CV", min_value=0.0, max_value=1000.0, value=0.0)
     with col2:
-        rdw_sd = st.number_input("RDW-SD", 0, 100, 0)
-        vcm = st.number_input("VCM", 0, 100, 0)
-        basophils = st.number_input("basophils", 0, 100, 0)
-        eosinophils = st.number_input("eosinophils", 0, 100, 0)
+        rdw_sd = st.number_input(label="RDW-SD", min_value=0.0, max_value=1000.0, value=0.0)
+        vcm = st.number_input(label="VCM", min_value=0.0, max_value=1000.0, value=0.0)
+        basophils = st.number_input(label="Basophils", min_value=0.0, max_value=1000.0, value=0.0)
+        eosinophils = st.number_input("Eosinophils", min_value=0.0, max_value=1000.0, value=0.0)
     with col3:
-        erythroblasts = st.number_input("erythroblasts", 0, 100, 0)
-        erythrocytes = st.number_input("erythrocytes", 0, 100, 0)
-        hematocrit = st.number_input("hematocrit", 0, 100, 0)
-        leukocytes = st.number_input("leukocytes", 0, 100, 0)
+        erythroblasts = st.number_input(label="Erythroblasts", min_value=0.0, max_value=1000.0, value=0.0)
+        erythrocytes = st.number_input(label="Erythrocytes", min_value=0.0, max_value=1000.0, value=0.0)
+        hematocrit = st.number_input(label="Hematocrit", min_value=0.0, max_value=1000.0, value=0.0)
+        leukocytes = st.number_input(label="Leukocytes", min_value=0.0, max_value=1000.0, value=0.0)
     with col4:
-        lymphocytes = st.number_input("lymphocytes", 0, 100, 0)
-        monocytes = st.number_input("monocytes", 0, 100, 0)
-        neutrophils = st.number_input("neutrophils", 0, 100, 0)
-        DAY_DIFFERENCE = st.number_input("DAY_DIFFERENCE", 0, 100, 0)
+        lymphocytes = st.number_input(label="Lymphocytes", min_value=0.0, max_value=1000.0, value=0.0)
+        monocytes = st.number_input(label="Monocytes", min_value=0.0, max_value=1000.0, value=0.0)
+        neutrophils = st.number_input(label="Neutrophils", min_value=0.0, max_value=1000.0, value=0.0)
+        DAY_DIFFERENCE = st.number_input(label="Days Since Symptoms", min_value=0.0, max_value=1000.0, value=0.0)
 
     NRS_values = {
-        "Note Red Series_Anisocitose +": 0,
-        "Note Red Series_Anisocitose ++": 0,
-        "Note Red Series_Anisocitose +++": 0,
-        "Note Red Series_Eritrócitos normais em tamanho": 0,
-        "Note Red Series_Macrocitose +": 0,
-        "Note Red Series_Macrocitose ++": 0,
-        "Note Red Series_Macrocitose +++": 0,
-        "Note Red Series_Microcitose +": 0,
-        "Note Red Series_Microcitose ++": 0,
-        "Note Red Series_Microcitose +++": 0,
+        "Anisocitose +": 0,
+        "Anisocitose ++": 0,
+        "Anisocitose +++": 0,
+        "Erythrocytes normal in size": 0,
+        "Macrocitose +": 0,
+        "Macrocitose ++": 0,
+        "Macrocitose +++": 0,
+        "Microcitose +": 0,
+        "Microcitose ++": 0,
+        "Microcitose +++": 0,
     }
-    NRS_option = st.selectbox("Select Note Red Series observation", NRS)
-    NRS_values[NRS_option] = 1;
+    NRS_option = st.selectbox("Note Red Series observation", list(NRS_values.keys()))
+    NRS_values[NRS_option] = 1
 
     submitted = st.form_submit_button("Submit")
-
-classifier = pickle.load(open("covid-xgb.pickle.dat", "rb"))
 
 predict_data = [hcm, hemoglobin, mchc, rdw_cv, rdw_sd, vcm, basophils,
                 eosinophils, erythroblasts, erythrocytes, hematocrit,
                 leukocytes, lymphocytes, monocytes, neutrophils,
-                DAY_DIFFERENCE, NRS_values['Note Red Series_Anisocitose +'],
-                NRS_values['Note Red Series_Anisocitose ++'],
-                NRS_values['Note Red Series_Anisocitose +++'],
-                NRS_values['Note Red Series_Eritrócitos normais em tamanho'],
-                NRS_values['Note Red Series_Macrocitose +'],
-                NRS_values['Note Red Series_Macrocitose ++'],
-                NRS_values['Note Red Series_Macrocitose +++'],
-                NRS_values['Note Red Series_Microcitose +'],
-                NRS_values['Note Red Series_Microcitose ++'],
-                NRS_values['Note Red Series_Microcitose +++']]
+                DAY_DIFFERENCE, NRS_values['Anisocitose +'],
+                NRS_values['Anisocitose ++'],
+                NRS_values['Anisocitose +++'],
+                NRS_values['Erythrocytes normal in size'],
+                NRS_values['Macrocitose +'],
+                NRS_values['Macrocitose ++'],
+                NRS_values['Macrocitose +++'],
+                NRS_values['Microcitose +'],
+                NRS_values['Microcitose ++'],
+                NRS_values['Microcitose +++']]
 
-prediction = classifier.predict(numpy.array(predict_data).reshape((1, -1)))
 
-st.success("Covid-19 Negative" if prediction[0] == 0 else "Covid-19 Positive")
+@st.cache(show_spinner=False)
+def predict(data):
+    with st.spinner('Predicting...'):
+        classifier = pickle.load(open("covid-xgb.pickle.dat", "rb"))
+        probability = classifier.predict_proba(numpy.array(data).reshape((1, -1)))
+    return probability
+
+
+if submitted:
+    prediction_proba = predict(predict_data)
+    if prediction_proba[0][0] > 0.5:
+        st.success("Covid-19 Negative")
+        st.info(f"Probability: {prediction_proba[0][0] * 100}")
+    else:
+        st.success("Covid-19 Positive")
+        st.info(f"Probability: {prediction_proba[0][1] * 100}")
+else:
+    st.write('Fill the values and click on "Submit" to predict covid-19 status.')
